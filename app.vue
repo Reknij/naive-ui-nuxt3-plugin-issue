db34c646eba5119f0b57c94ea7504da90e169c18
Reknij <jinker2003@gmail.com>
2024-04-15 12:33:34 +0800

    first code

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { NText, NCard, NSpace, NButton } from "naive-ui";
+</script>
+
+<template>
+  <div>
+    <NCard>
+      <NSpace vertical align="center">
+        <NText type="success">Hello, World!</NText>
+        <NButton>Click me.</NButton>
+      </NSpace>
+    </NCard>
+  </div>
+</template>
